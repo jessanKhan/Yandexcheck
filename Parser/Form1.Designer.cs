113f6ddd9e15b6db8cf74b678961df30b43bfd1b
Mahdi <mahdi@m.insta.kim>
2018-10-08 20:23:15 +0600

    work is done
    Need to design the ui

--- Parser/Form1.Designer.cs
+++ Parser/Form1.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.txtLog = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.srchbtn = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.resultct = new System.Windows.Forms.TextBox();
+            this.nf = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(394, 10);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(174, 23);
             this.button1.TabIndex = 0;
@@ -48,40 +48,20 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // txtLog
-            // 
-            this.txtLog.AcceptsReturn = true;
-            this.txtLog.AcceptsTab = true;
-            this.txtLog.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
-            this.txtLog.Location = new System.Drawing.Point(13, 257);
-            this.txtLog.Multiline = true;
-            this.txtLog.Name = "txtLog";
-            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLog.Size = new System.Drawing.Size(837, 272);
-            this.txtLog.TabIndex = 1;
-            this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
-            this.label1.Location = new System.Drawing.Point(395, 203);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Counter Found";
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(192, 14);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(657, 20);
+            this.textBox1.Size = new System.Drawing.Size(376, 20);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(193, 116);
+            this.textBox2.Location = new System.Drawing.Point(192, 116);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(207, 20);
             this.textBox2.TabIndex = 4;
@@ -89,44 +69,63 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(90, 116);
+            this.label2.Location = new System.Drawing.Point(73, 119);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Counter ID";
             // 
-            // button2
+            // srchbtn
             // 
-            this.button2.Location = new System.Drawing.Point(420, 114);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Search";
-            this.button2.UseVisualStyleBackColor = true;
+            this.srchbtn.Location = new System.Drawing.Point(420, 114);
+            this.srchbtn.Name = "srchbtn";
+            this.srchbtn.Size = new System.Drawing.Size(75, 23);
+            this.srchbtn.TabIndex = 6;
+            this.srchbtn.Text = "Search";
+            this.srchbtn.UseVisualStyleBackColor = true;
+            this.srchbtn.Click += new System.EventHandler(this.srchbtn_Click);
             // 
-            // label3
+            // richTextBox
             // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.DarkRed;
-            this.label3.Location = new System.Drawing.Point(395, 216);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Counter Not Found";
+            this.richTextBox.Location = new System.Drawing.Point(13, 258);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ReadOnly = true;
+            this.richTextBox.Size = new System.Drawing.Size(555, 133);
+            this.richTextBox.TabIndex = 8;
+            this.richTextBox.Text = "";
+            // 
+            // resultct
+            // 
+            this.resultct.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultct.ForeColor = System.Drawing.Color.Green;
+            this.resultct.Location = new System.Drawing.Point(192, 160);
+            this.resultct.Name = "resultct";
+            this.resultct.Size = new System.Drawing.Size(100, 29);
+            this.resultct.TabIndex = 9;
+            // 
+            // nf
+            // 
+            this.nf.BackColor = System.Drawing.Color.White;
+            this.nf.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nf.ForeColor = System.Drawing.Color.Red;
+            this.nf.Location = new System.Drawing.Point(299, 160);
+            this.nf.Name = "nf";
+            this.nf.Size = new System.Drawing.Size(100, 29);
+            this.nf.TabIndex = 10;
             // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(862, 702);
+            this.ClientSize = new System.Drawing.Size(581, 413);
+            this.Controls.Add(this.nf);
+            this.Controls.Add(this.resultct);
+            this.Controls.Add(this.richTextBox);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.srchbtn);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtLog);
             this.Controls.Add(this.button1);
             this.ImeMode = System.Windows.Forms.ImeMode.On;
             this.Name = "Form1";
@@ -140,13 +139,13 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox txtLog;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button srchbtn;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.TextBox resultct;
+        private System.Windows.Forms.TextBox nf;
     }
 }
 
--- Parser/Form1.cs
+++ Parser/Form1.cs
@@ -13,6 +13,7 @@ namespace Parser
 {
     public partial class Form1 : Form
     {
+        //int t = 0;
         string ul;
         public Form1()
         {
@@ -23,10 +24,84 @@ namespace Parser
         private void button1_Click(object sender, EventArgs e)
         {
             ul = textBox1.Text;
-            txtLog.Text = Parser(ul);
+            richTextBox.Text = Parser(ul);
+            
         }
 
 
+        private void search_Click(object sender, EventArgs e)
+        {
+            //int index = 0;
+            //String temp = richTextBox.Text;
+
+            //richTextBox.Text = temp;
+            //while (index < richTextBox.Text.LastIndexOf(textBox2.Text))
+
+            //{
+            //    richTextBox.Find(textBox2.Text, index, richTextBox.TextLength, RichTextBoxFinds.None);
+            //    richTextBox.SelectionBackColor = System.Drawing.Color.Red;
+            //    index = richTextBox.Text.IndexOf(textBox2.Text, index) + 1;
+            //}
+
+        }
+
+
+        //private void search_Click(object sender, EventArgs e)
+        //{
+        //    for (int i = t; i < richTextBox.TextLength - textBox2.TextLength; i++)
+        //    {
+        //        string x = "";
+        //        for (int j = 0; j < textBox2.TextLength; j++)
+        //        {
+        //            if (textBox2.Text[j] == richTextBox.Text[i + j])
+        //            {
+        //                x += richTextBox.Text[i + j] + "";
+
+        //            }
+
+        //            else x = "";
+        //        }
+        //        if (x == textBox2.Text)
+        //        {
+
+        //            t = i + 1;
+        //            richTextBox.SelectAll();
+        //            richTextBox.SelectionBackColor = System.Drawing.Color.Green;
+        //            richTextBox.Select(i, textBox2.TextLength);
+        //            richTextBox.SelectionBackColor = System.Drawing.Color.Blue;
+        //            break;
+
+        //        }
+        //    }
+
+
+        //}
+
+
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    string[] counter = textBox2.Text.Split(',');
+        //    foreach (string word in counter)
+        //    {
+        //        int startIndex = 0;
+        //        while (startIndex < richTextBox.TextLength)
+        //        {
+        //            int wordStartIndex = richTextBox.Find(word, startIndex, RichTextBoxFinds.None);
+        //            if (wordStartIndex != -1)
+        //            {
+        //                richTextBox.SelectionStart = wordStartIndex;
+        //                richTextBox.SelectionLength = word.Length;
+        //                richTextBox.SelectionBackColor = System.Drawing.Color.Yellow;
+
+
+        //            }
+        //            else
+        //                break;
+        //            startIndex += wordStartIndex + word.Length;
+        //        }
+        //    }
+        //}
 
         public string GetHtml(string url)
         {
@@ -66,16 +141,49 @@ namespace Parser
             return output.ToString();
         }
 
-        
-        
 
-        private void txtLog_TextChanged(object sender, EventArgs e)
+        //private void richTextBox_TextChanged(object sender, EventArgs e)
+        //{
+
+        //}
+
+       
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void srchbtn_Click(object sender, EventArgs e)
         {
+            int index = 0;
+            String temp = richTextBox.Text;
+
+            richTextBox.Text = temp;
+            while (index < richTextBox.Text.LastIndexOf(textBox2.Text))
+
+            {
+                richTextBox.Find(textBox2.Text, index, richTextBox.TextLength, RichTextBoxFinds.None);
+                richTextBox.SelectionBackColor = System.Drawing.Color.Red;
+                index = richTextBox.Text.IndexOf(textBox2.Text, index) + 1;
+                
+
+            }
+            if (richTextBox.SelectionBackColor == System.Drawing.Color.Red)
+            {
+                nf.Text = "";
+                resultct.Text = "Found";
+
+                //if (richTextBox.SelectionBackColor != System.Drawing.Color.Red)
+                //{
+
+                //}
+            }
+            else
+            {
+                nf.Text = "Not Found";
+                resultct.Text = "";
+            }
 
         }
     }
